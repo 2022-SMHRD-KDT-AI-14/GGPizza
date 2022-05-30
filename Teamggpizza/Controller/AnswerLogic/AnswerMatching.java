7c8e2b513ecc0479c45fe1d20d5d1c19fa05e6d1
smhrd <smhrd@211.228.61.200>
2022-05-30 19:00:56 +0900

    AnswerLogic - AnswerMatching level 5 for문 추가(시안)

--- Teamggpizza/Controller/AnswerLogic/AnswerMatching.java
+++ Teamggpizza/Controller/AnswerLogic/AnswerMatching.java
@@ -24,101 +24,117 @@ public class AnswerMatching extends StartingSub {
 					}
 				}
 			}
-			
+
 			if (nowLevel == 48) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("버섯")) {
-						money += 10500;
+					for (int j = 0; j < i; j++) {
+						int cnt = 0;
+						if (ans.answer.get(j).equals("소스")) {
+							cnt++;
+						}
+						if (ans.answer.get(j).equals("버섯")) {
+							cnt++;
+						}
+						if (cnt == 2) {
+							money += 10500;
+						}
 					}
 				}
 			}
-			
+
 			if (49 <= nowLevel && nowLevel <= 50) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈")
+							&& ans.answer.get(i).equals("버섯")) {
 						money += 14500;
 					}
 				}
 			}
-			
+
 			if (nowLevel == 51) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("페퍼로니") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("페퍼로니")
+							&& ans.answer.get(i).equals("버섯")) {
 						money += 16500;
 					}
 				}
 			}
-			
+
 			if (nowLevel == 52) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("피망") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("피망")
+							&& ans.answer.get(i).equals("버섯")) {
 						money += 16500;
 					}
 				}
 			}
-			
+
 			if (nowLevel == 53) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("소시지") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("소시지")
+							&& ans.answer.get(i).equals("버섯")) {
 						money += 17000;
 					}
 				}
 			}
-			
+
 			if (54 <= nowLevel && nowLevel <= 56) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈") && ans.answer.get(i).equals("페퍼로니") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈")
+							&& ans.answer.get(i).equals("페퍼로니") && ans.answer.get(i).equals("버섯")) {
 						money += 20500;
 					}
 				}
 			}
-			
+
 			if (57 <= nowLevel && nowLevel <= 58) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈") && ans.answer.get(i).equals("피망") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈")
+							&& ans.answer.get(i).equals("피망") && ans.answer.get(i).equals("버섯")) {
 						money += 20500;
 					}
 				}
 			}
-			
+
 			if (59 <= nowLevel && nowLevel <= 60) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈") && ans.answer.get(i).equals("소시지") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈")
+							&& ans.answer.get(i).equals("소시지") && ans.answer.get(i).equals("버섯")) {
 						money += 21000;
 					}
 				}
 			}
-			
+
 			if (nowLevel == 61) { // 현재 array Random 할당되는 값에 따라서 nowLevel에 값 할당
 				for (int i = 0; i < ans.answer.size(); i++) {
 					// 토핑추가에 답한 만큼 비교해서 아래 답과 일치하면 money 추가
 					// 추가로 비교할 토핑 많아지면 if-else나 &&로 다중 처리
-					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈") && ans.answer.get(i).equals("페퍼로니") && ans.answer.get(i).equals("피망") && ans.answer.get(i).equals("소시지") && ans.answer.get(i).equals("버섯")) {
+					if (ans.answer.get(i).equals("소스") && ans.answer.get(i).equals("치즈")
+							&& ans.answer.get(i).equals("페퍼로니") && ans.answer.get(i).equals("피망")
+							&& ans.answer.get(i).equals("소시지") && ans.answer.get(i).equals("버섯")) {
 						money += 33000;
 					}
 				}
 			}
-			
+
 		} // level == 5 end
 
 	}
-	
+
 }
-
-
