488d1a11f9e87277e30aea062f055ad439668b3d
smhrd <smhrd@218.157.24.40>
2022-05-31 14:37:05 +0900

    method 인터페이스화 _ 하진수

--- Teamggpizza/Controller/AnswerLogic/AnswerMatching.java
+++ Teamggpizza/Controller/AnswerLogic/AnswerMatching.java
@@ -10,14 +10,5 @@ public class AnswerMatching extends StartingSub {
 	Answer ans = new Answer();
 	Day1_Question day1 = new Day1_Question();
 	RandomQuestion rdQuest = new RandomQuestion();
-	AwCheck awcheck = new AwCheck();
 	
-	public void answerMT_day1() {
-		awcheck.Level1();
-		awcheck.Level2();
-		awcheck.Level3();
-		awcheck.Level4();
-		awcheck.Level5();
-	}
-
 }
