53e54e42233a3d4d94673f6d22cac578ff462c0d
smhrd <smhrd@218.157.24.40>
2022-05-31 18:32:53 +0900

    day와 중간정산 구현_하진수

--- Teamggpizza/Controller/StartSupport/Assembly.java
+++ Teamggpizza/Controller/StartSupport/Assembly.java
@@ -17,7 +17,6 @@ public class Assembly extends StartingSub implements Tutorial {
 	int cnt;
 	String enter;
 
-
 	@Override
 	public void manual() {
 		TutorialScanner tutosc = new TutorialScanner();
@@ -33,12 +32,11 @@ public class Assembly extends StartingSub implements Tutorial {
 		manual.tutorial();
 		manual.manual();
 
-
 		/*
 		 * 수정하는 부분들을 위해 편의상 메뉴얼 UI만 존재할 뿐 Starting에는 할당되지 않음 tutorial과 manual 출력은
 		 * accountSet method 내 switch문 case 1에 존재함
 		 */
-		
+
 	}
 
 	@Override
@@ -51,12 +49,12 @@ public class Assembly extends StartingSub implements Tutorial {
 		Day1_Question dayList = new Day1_Question();
 		AnswerMatching ansMat = new AnswerMatching();
 		AwCheck ach = new AwCheck();
-		
+
 		// view에서 처음으로 보여지는 tutorial part
-		
+
 		tutosc.firstView();
 		// 첫 시작 메뉴와 스캐너로 number에 수를 할당
-		
+
 		startnum.numberOf1();
 		startnum.numberOf2();
 		startnum.numberOf3();
@@ -64,14 +62,14 @@ public class Assembly extends StartingSub implements Tutorial {
 		 * 스캔 받은 number에 따라 다르게 전개되는 start 구간 모두 startSelectNumber.class 안에 있는 method
 		 * 이름에서 보여지듯 start 부분만 할당되고 해당 number에 method를 부여 다음 chapter로 연결할 예정
 		 */
-		
+
 		dayList.day1Quest();
 		manual();
-		
+
 	}
-	
+
 	public void Game() {
-		
+
 		TutorialScanner tutosc = new TutorialScanner();
 		StartSelectNumber startnum = new StartSelectNumber();
 		RandomQuestion rdQuest = new RandomQuestion();
@@ -81,8 +79,8 @@ public class Assembly extends StartingSub implements Tutorial {
 		AnswerMatching ansMat = new AnswerMatching();
 		AwCheck ach = new AwCheck();
 
-		level+=1;
-		
+		level += 1;
+
 		while (cnt != 25) {
 
 			// 본 게임 시작
@@ -90,6 +88,16 @@ public class Assembly extends StartingSub implements Tutorial {
 			ans.answerScanner(); // 질문에 대한 토핑 답하는 method
 			ach.levels(); // 정답 매칭 시스템
 			cnt++;
+
+			if (cnt % 5 == 0) {
+
+				day++;
+				System.out.println();
+				System.out.println("Day : " + day);
+				System.out.println(day + "번째 날 중간 정산 : " + money);
+
+			}
+
 			ans.answer.clear();
 			System.out.println();
 			System.out.println("Cnt view Test : " + cnt);
